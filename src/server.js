bd0e6972b11137fa7b4cb8f80f948b978432d50f
Kye <NovusAa12@proton.me>
2024-01-23 12:01:08 +0000

    update server.js

--- src/server.js
+++ src/server.js
@@ -1,23 +1,23 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const cors = require('cors');
-const morgan = require('morgan');
+const cors = require("cors");
+const morgan = require("morgan");
 
-app.disable('x-powered-by');
+app.disable("x-powered-by");
 
 // Add middleware
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
-
 // Add your router below
 
+const screensRouter = require("./routers/screens.router");
+app.use("/screens", screensRouter);
 
+const ticketsRouter = require("./routers/ticket.router");
+app.use("/tickets", ticketsRouter);
 
-
-
-module.exports = app
+module.exports = app;
